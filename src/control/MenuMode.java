df99a198f2923c4b1ea1024e536d9a9f693ab142
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-04 20:48:57 +0200

    "Arreglado"

--- src/control/MenuMode.java
+++ src/control/MenuMode.java
@@ -71,7 +71,7 @@ public class MenuMode implements Mode{
 		commands = new HashMap<>();
 	
 		commands.put('a', new ValidateChallengeCommand(context, userManager, challengeMediator, authManager));
-		commands.put('b', new AdminEditCharacterCommand(context, userManager));
+		commands.put('b', new EditCharacterCommand(context, userManager));
 		commands.put('c', new ManageUsersCommand(context, userManager));
 		commands.put('d', new CheckRankingCommand(context, userManager, authManager, challengeMediator));
 		commands.put('e', new UnregisterCommand(context, userManager, authManager, challengeMediator));
